6f592da4cb8aee062f6e77cc4f5c908f95e2e173
FenFirasy <firasy720@gmail.com>
2026-04-24 23:24:31 +0700

    add findall

--- backend/src/main/java/com/leadestate/backend/controller/LeadController.java
+++ backend/src/main/java/com/leadestate/backend/controller/LeadController.java
@@ -1,5 +1,22 @@
 package com.leadestate.backend.controller;
 
+import com.leadestate.backend.entity.Lead;
+import com.leadestate.backend.repository.LeadRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/leads")
 public class LeadController {
-    
+
+    @Autowired
+    private LeadRepository leadRepository;
+    @GetMapping
+    public List<Lead> getAllLeads() {
+        return leadRepository.findAll();
+    }
 }
